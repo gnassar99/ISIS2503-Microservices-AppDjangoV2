fd533588c9b8e3fdc2aadd996ba504ffd8036fe1
Gabriel Nassar <g.nassar@uniandes.edu.co>
2023-05-21 11:53:42 -0500

    Updated

--- places/monitoring/settings.py
+++ places/monitoring/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'places_db',
         'USER': 'places_user',
         'PASSWORD': 'isis2503',
-        'HOST': '10.128.0.7',
+        'HOST': '10.128.0.9',
         'PORT': '5432',
     }
 }
